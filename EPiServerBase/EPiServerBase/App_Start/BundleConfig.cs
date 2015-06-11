7a51ed1c087051310678f7a75fb794410d492480
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-06-11 17:09:05 +0200

    Added Script and Styles bundle

---
+++ EPiServerBase/EPiServerBase/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+
+namespace EPiServerBase.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/bundles/bootstrap")
+                .Include("~/Content/bootstrap.css"));
+        }
+    }
+}
--- EPiServerBase/EPiServerBase/Global.asax.cs
+++ EPiServerBase/EPiServerBase/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Web.Optimization;
+using EPiServerBase.App_Start;
 
 namespace EPiServerBase
 {
@@ -8,8 +10,7 @@ namespace EPiServerBase
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            //Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
