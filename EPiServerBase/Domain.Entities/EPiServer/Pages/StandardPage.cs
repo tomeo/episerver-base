bb417c04be6b8b999e7c9aa1d5905b12a0d65015
Tommy Ivarsson <tommy.ivarsson@capgemini.com>
2015-06-12 17:06:02 +0200

    Added StandardPage ContentType

---
+++ EPiServerBase/Domain.Entities/EPiServer/Pages/StandardPage.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace Domain.Entities.EPiServer.Pages
+{
+    [ContentType(
+        DisplayName = "StandardPage",
+        GUID = "1d1eb7b5-6f17-4790-90f8-4b1cad937ab4",
+        Description = "")]
+    public class StandardPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main intro",
+            Description = "",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual string MainIntro { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description =
+                "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
---
+++ EPiServerBase/EPiServerBase/Controllers/StandardPageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Domain.Entities.EPiServer.Pages;
+using EPiServer.Web.Mvc;
+
+namespace EPiServerBase.Controllers
+{
+    public class StandardPageController : PageController<StandardPage>
+    {
+        public ActionResult Index(StandardPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
